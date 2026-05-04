cc2c72ce6c4b85b4ebd453b54a471dada91d6ea4
Nicole Marcatoma <nicolemarcatoma2026@u.northwestern.edu>
2026-05-04 02:04:44 -0500

    success test

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ def build_model(preprocessor):
     model = Pipeline(steps=[
         ("preprocessor", preprocessor),
         ("model", RandomForestRegressor(
-            n_estimators=300,
+            n_estimators=400,
             min_samples_leaf=1,
             max_features=0.7,
             random_state=390,
